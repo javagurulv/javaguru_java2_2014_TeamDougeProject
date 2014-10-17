49196748b9e485595a9258c55f93695443b96b84
Juris <jura632@inbox.lv>
2014-10-17 12:26:56 +0300

    Actor class

---
+++ douge/src/main/java/lv/javaguru/java2/domain/Actor.java
@@ -0,0 +1,45 @@
+package lv.javaguru.java2.domain;
+
+import java.util.Date;
+
+/**
+ * Created by Juris on 17.10.2014.
+ */
+public class Actor {
+    private Long actor_id;
+    private String first_name;
+    private String last_name;
+    private Date last_update;
+
+    public Long getActor_id() {
+        return actor_id;
+    }
+
+    public void setActor_id(Long actor_id) {
+        this.actor_id = actor_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Date last_update) {
+        this.last_update = last_update;
+    }
+}
