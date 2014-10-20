9028a4a65a519bd01f980adc327ceb8e6cff4822
radchuksergey <radchuk.sergey@gmail.com>
2014-10-20 12:37:02 +0300

    20.10.2014 Radchuk Sergey StoreDAOImplTest checked

--- douge/src/test/java/lv/javaguru/java2/database/jdbc/StoreDAOImplTest.java
+++ douge/src/test/java/lv/javaguru/java2/database/jdbc/StoreDAOImplTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 
 public class StoreDAOImplTest {
@@ -54,5 +55,65 @@ public class StoreDAOImplTest {
         assertEquals(store.getManager_staff_id(), storeFromDB.getManager_staff_id());
         assertionEqualsDateCustom(store.getLast_update(), storeFromDB.getLast_update());
 
+    }
+
+    @Test
+    public void testUpdate() throws DBException
+    {
+        Store store = createStore(12,15);
+        storeDAO.create(store);
+        store.setAddress_id(88);
+        store.setManager_staff_id(99);
+        storeDAO.update(store);
+
+        Store storeFromDB = storeDAO.getByID(store.getStore_id());
+        assertNotNull(storeFromDB);
+        assertEquals(store.getAddress_id(), storeFromDB.getAddress_id());
+        assertEquals(store.getManager_staff_id(), storeFromDB.getManager_staff_id());
+        assertionEqualsDateCustom(store.getLast_update(), storeFromDB.getLast_update());
+    }
+
+    @Test
+    public void testDelete() throws DBException
+    {
+        Store store = createStore(12,15);
+        storeDAO.create(store);
+        storeDAO.delete(store.getStore_id());
+
+        Store storeFromDB = storeDAO.getByID(store.getStore_id());
+        assertNull(storeFromDB);
+
+    }
+
+    @Test
+    public void testGetAll() throws DBException
+    {
+        Store store = createStore(12,15);
+        storeDAO.create(store);
+        Store store1 = createStore(13,18);
+        storeDAO.create(store1);
+        List<Store> storeList = storeDAO.getAll();
+        assertEquals(storeList.size(), 2);
+    }
+
+    @Test
+    public void testGetAllByAddressId() throws DBException
+    {
+        Store store = createStore(12,15);
+        storeDAO.create(store);
+        Store store1 = createStore(13,18);
+        storeDAO.create(store1);
+        List<Store> storeList = storeDAO.getAllByAddressId(12);
+        assertEquals(storeList.size(), 1);
+    }
+
+    public void testGetAllByManagerStaffId() throws DBException
+    {
+        Store store = createStore(12,15);
+        storeDAO.create(store);
+        Store store1 = createStore(13,18);
+        storeDAO.create(store1);
+        List<Store> storeList = storeDAO.getAllByManagerStaffID(18);
+        assertEquals(storeList.size(), 1);
     }
 }
