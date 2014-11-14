871df90d2afb74cbae1dda740fd2eace23f79c23
Juris <jura632@inbox.lv>
2014-11-14 19:18:03 +0200

    Index -> login

--- douge/src/main/java/lv/javaguru/java2/servlets/mvc/controllrers/IndexController.java
+++ douge/src/main/java/lv/javaguru/java2/servlets/mvc/controllrers/IndexController.java
@@ -1,28 +1,66 @@
 package lv.javaguru.java2.servlets.mvc.controllrers;
 
+import lv.javaguru.java2.database.DBException;
+import lv.javaguru.java2.database.UserDAO;
+import lv.javaguru.java2.database.jdbc.UserDAOImpl;
+import lv.javaguru.java2.domain.User;
 import lv.javaguru.java2.servlets.mvc.MVCController;
 import lv.javaguru.java2.servlets.mvc.models.MVCModel;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Juris on 09.11.2014.
  */
 public class IndexController implements MVCController {
+
+    private UserDAO userDAO = new UserDAOImpl();
+
     @Override
-    public MVCModel processRequest(HttpServletRequest request, HttpServletResponse response) {
+    public MVCModel processRequest(HttpServletRequest req, HttpServletResponse resp) {
 
+        String Model = "/jsp/index.jsp";
+        String ErrorString = "";
 
-        String indexPageCode;
+        //check that submit button was pressed and POST data received
+        if (req.getParameter("submit") != null) {
 
-        indexPageCode="<br>" + "<br>" + "\n" +
-                      "<h3><a href=\"/login\">Sign in</h3>" + "\n" +
-                      "<h3><a href=\"/adduser\">Add User</h3>" + "\n" +
-                      "<h3><a href=\"/films\">Show film list</a></h3>" + "\n" +
-                      "<h3><a href=\"/actors\">Show actor list</a></h3>" + "\n";
+            //check that fields are not empty
+            if (isParametersValid(req)) {
+                String login = req.getParameter("login");
+                String password = req.getParameter("passwd");
 
+                User userFromDB;
 
-        return new MVCModel("/jsp/index.jsp", indexPageCode);
+                try {
+                    userFromDB = userDAO.getByLogin(login);
+
+                    //check that login exists and password matches
+                    if (userFromDB != null && password.equals(userFromDB.getPassword())) {
+                        HttpSession session = req.getSession();
+                        session.setAttribute("sessionLogin", login);
+                        session.setMaxInactiveInterval(300);
+
+                        Model = "/jsp/securearea.jsp";
+                    } else {
+                        ErrorString = "<font color=\"red\">Login and/or password incorrect!</font>";
+                        Model ="/jsp/login.jsp";
+                    }
+                } catch (DBException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                ErrorString = "<font color=\"red\">Name and/or password can't be empty!</font>";
+                Model ="/jsp/login.jsp";
+            }
+        }
+        return new MVCModel(Model, ErrorString);
+    }
+
+    private boolean isParametersValid(HttpServletRequest req) {
+        return !req.getParameter("login").trim().isEmpty() &&
+                !req.getParameter("passwd").trim().isEmpty();
     }
 }
