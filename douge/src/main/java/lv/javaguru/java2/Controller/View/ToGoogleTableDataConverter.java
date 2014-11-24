14e56e5cb3e936278aee0eca971bd9f117cd2b8b
Juris <jura632@inbox.lv>
2014-11-24 20:04:25 +0200

    Actor

--- douge/src/main/java/lv/javaguru/java2/Controller/View/ToGoogleTableDataConverter.java
+++ douge/src/main/java/lv/javaguru/java2/Controller/View/ToGoogleTableDataConverter.java
@@ -5,10 +5,12 @@ import com.google.visualization.datasource.datatable.DataTable;
 import com.google.visualization.datasource.render.HtmlRenderer;
 import com.ibm.icu.util.ULocale;
 import lv.javaguru.java2.Controller.TableData;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Juris on 24.11.2014.
  */
+@Component("ToGoogleTableDataConverter")
 public class ToGoogleTableDataConverter extends TableDataToGoogleWEBTableConverter {
 
     public DataTable convertDataTableToGoogleTable(TableData tableData) throws TypeMismatchException {
