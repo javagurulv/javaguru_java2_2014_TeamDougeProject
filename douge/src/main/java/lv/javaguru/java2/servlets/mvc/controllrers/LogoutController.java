f1dae520b09730c2db7b1bf289614eda027c77a9
Juris <jura632@inbox.lv>
2014-12-19 20:04:29 +0200

    logout MVC -> Spring MVC

--- douge/src/main/java/lv/javaguru/java2/servlets/mvc/controllrers/LogoutController.java
+++ douge/src/main/java/lv/javaguru/java2/servlets/mvc/controllrers/LogoutController.java
@@ -3,6 +3,9 @@ package lv.javaguru.java2.servlets.mvc.controllrers;
 import lv.javaguru.java2.servlets.mvc.MVCController;
 import lv.javaguru.java2.servlets.mvc.models.MVCModel;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,17 +15,21 @@ import javax.servlet.http.HttpSession;
  * Created by user on 14-Nov-14.
  */
 @Component
-public class LogoutController implements MVCController {
-    public MVCModel processRequest(HttpServletRequest req, HttpServletResponse resp) {
+@RequestMapping(value = "logout", method = {RequestMethod.GET,RequestMethod.POST})
+public class LogoutController{
+    public ModelAndView processRequest(HttpServletRequest req, HttpServletResponse resp) {
 
+        ModelAndView model = new ModelAndView();
+        model.setViewName("logout");
         HttpSession session = req.getSession(false);
 
         //invalidate the session if exists
         if (session != null) {
             session.invalidate();
-            return new MVCModel("/jsp/logout.jsp", 1); //logout and redirect
+            model.addObject("model",1);
         } else {
-            return new MVCModel("/jsp/logout.jsp", 0); //just redirect
+            model.addObject("model",0);
         }
+        return model;
     }
 }
