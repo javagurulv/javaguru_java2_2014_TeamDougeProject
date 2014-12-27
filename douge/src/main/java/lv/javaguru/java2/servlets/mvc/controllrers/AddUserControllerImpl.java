f6583b3c552c24dfe4ba713894d9bb12b3763865
Sergo <radchuk.sergey@gmail.com>
2014-12-27 11:58:47 +0200

    Add user controller  -> add MD5 operation

--- douge/src/main/java/lv/javaguru/java2/servlets/mvc/controllrers/AddUserControllerImpl.java
+++ douge/src/main/java/lv/javaguru/java2/servlets/mvc/controllrers/AddUserControllerImpl.java
@@ -79,8 +79,22 @@ public class AddUserControllerImpl {
 
         user.setComments(comments);
         user.setLogin(login);
-        user.setPassword(password);
+        user.setPassword(MD5(password));
         user.setUser_type(Long.parseLong(user_type));
         return user;
+    }
+
+    private String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
     }
 }
